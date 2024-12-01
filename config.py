206e2386995b5842016f020cab17ff6b3bb34425
mailcorahul <mailcorahul@gmail.com>
2024-12-01 20:43:01 +0530

    Add, test config params to merge and unload

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import torch
 
 TRAINING_CONFIGS = {
     "base_model_url": "google/gemma-2-9b-it",
-    "adapter_model_url": "gemma-2-9b-it-cnn_dailymail-v2",
+    "adapter_model_url": "gemma-2-9b-it-cnn_dailymail-adapter",
     "dataset_name": "abisee/cnn_dailymail",
     "wandb_project_name": "finetune gemma-2-9b-it",
     "attn_implementation" : "eager",
     "torch_dtype" : torch.bfloat16,
     "num_dataset_samples": 1000,
-    "num_epochs": 3,
+    "num_epochs": 1,
     "eval_steps": 5000,
     "dataset_shuffle_seed": 65,
     "test_size": 0.1,
@@ -17,4 +17,14 @@ TRAINING_CONFIGS = {
     "eval_steps": 5000,
     "device_map": "auto",
     "eval_metric": "rouge",
+}
+
+
+MERGE_ADAPTER_CONFIGS = {
+    "base_model_url": "google/gemma-2-9b-it",
+    "adapter_model_url": "gemma-2-9b-it-cnn_dailymail-adapter",
+    "new_model_url": "gemma-2-9b-it-cnn_dailymail-finetuned",
+    "device_map": "cpu",
+    "torch_dtype" : torch.bfloat16,
+    "attn_implementation" : "eager"
 }
--- merge_and_unload.py
+++ merge_and_unload.py
@@ -3,11 +3,14 @@ from peft import PeftModel
 import torch
 from trl import setup_chat_format
 
-base_model_url = "google/gemma-2-27b-it"
-new_model_url = "Gemma-2-27b-it-cnn_dailymail"
-save_dir = "Gemma-2-27b-it-cnn_dailymail-merged"
+from config import MERGE_ADAPTER_CONFIGS
 
-print("[/] loading base model and tokenizer")
+base_model_url = MERGE_ADAPTER_CONFIGS["base_model_url"]
+adapter_model_url = MERGE_ADAPTER_CONFIGS["adapter_model_url"]
+save_dir = MERGE_ADAPTER_CONFIGS["new_model_url"]
+
+
+print("[/] loading base model and tokenizer...")
 # Reload tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained(base_model_url)
 
@@ -15,14 +18,14 @@ base_model_reload = AutoModelForCausalLM.from_pretrained(
     base_model_url,
     low_cpu_mem_usage=True,
     return_dict=True,
-    torch_dtype=torch.bfloat16,
-    device_map="cpu",
-    attn_implementation="eager"
+    torch_dtype=MERGE_ADAPTER_CONFIGS["torch_dtype"],
+    device_map=MERGE_ADAPTER_CONFIGS["device_map"],
+    attn_implementation=MERGE_ADAPTER_CONFIGS["attn_implementation"]
 )
 
 print("[/] merging adapter weights with the base model...")
 # base_model_reload, tokenizer = setup_chat_format(base_model_reload, tokenizer)
-model = PeftModel.from_pretrained(base_model_reload, new_model_url)
+model = PeftModel.from_pretrained(base_model_reload, adapter_model_url)
 
 model = model.merge_and_unload()
 
